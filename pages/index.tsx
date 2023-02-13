ec55f6e82375c3b132e10bc5ef4c643fd7e4f8cb
odysa <cbian7@wisc.edu>
2023-02-13 17:44:39 +0000

    fix es error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import { Chat } from "../components/Chat";
 import Layout from "@/components/Layout";
 import { Intro } from "@/components/Intro";
+import Head from "next/head";
+
 function Home() {
   return (
     <Layout>
+      <Head>
+        <title>Bucky AI</title>
+      </Head>
       <Intro />
       <Chat />
     </Layout>
