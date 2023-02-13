35cd08bf8284cc1901ab95c7413780b49d09b158
odysa <piensengxv@gmail.com>
2023-02-13 20:23:24 +0800

    temp api

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,5 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function handler(req: NextRequest) {
+    const { question } = await req.json() as { question: string };
+
+    if (!question) {
+        return new NextResponse(JSON.stringify({ "answer": "Empty prompt" }),
+            {
+                status: 400,
+                headers: { 'content-type': 'application/json' },
+            })
+    }
+
+    const res = await fetch("https://bucky-ai-production.up.railway.app/qa", {
+        method: "GET"
+    });
+
+    if (res.status != 200) {
+        return new NextResponse(JSON.stringify({ "answer": "Failed to fetch answer" }),
+            {
+                status: 400,
+                headers: { 'content-type': 'application/json' },
+            })
+    }
+
+    const respJson = await res.json();
+
     return new Response(JSON.stringify({ "text": "jack" }))
 }
