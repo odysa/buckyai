cabd12b71ba35dd70bfd92f2bece8926ae67783d
odysa <cbian7@wisc.edu>
2023-02-13 10:06:53 +0000

    reset chatbot example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,24 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react";
-import type { Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { Provider as RWBProvider } from "react-wrap-balancer";
-import cx from "classnames";
-import localFont from "@next/font/local";
-import { Inter } from "@next/font/google";
+import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
+import type { LayoutProps } from '@vercel/examples-ui/layout'
 
-const sfPro = localFont({
-  src: "../styles/SF-Pro-Display-Medium.otf",
-  variable: "--font-sf",
-});
+import { getLayout } from '@vercel/examples-ui'
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
+import '@vercel/examples-ui/globals.css'
 
-export default function MyApp({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps<{ session: Session }>) {
+function App({ Component, pageProps }: AppProps) {
+  const Layout = getLayout<LayoutProps>(Component)
+
   return (
-    <SessionProvider session={session}>
-      <RWBProvider>
-        <div className={cx(sfPro.variable, inter.variable)}>
-          <Component {...pageProps} />
-        </div>
-      </RWBProvider>
+    <Layout
+      title="ai-chatgpt"
+      path="solutions/ai-chatgpt"
+      description="ai-chatgpt"
+    >
+      <Component {...pageProps} />
       <Analytics />
-    </SessionProvider>
-  );
+    </Layout>
+  )
 }
+
+export default App
